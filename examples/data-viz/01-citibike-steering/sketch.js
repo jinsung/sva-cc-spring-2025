76a11217c1088ec9966f902628bcf084fe4f4b5c
Jin Sung Yoo <jinsungy@google.com>
2025-02-01 16:09:00 -0500

    add data viz project

---
+++ examples/data-viz/01-citibike-steering/sketch.js
@@ -0,0 +1,90 @@
+const date = "2023-11-06";
+let data;
+let stations = [];
+let trips = [];
+let maxLat, minLat, maxLng, minLng;
+let currentTime;
+let maxTime;
+let timeSlider;
+let isPlaying = true;
+
+function preload() {
+  data = loadJSON("../data/" + date + ".json");
+}
+
+function setup() {
+  colorMode(HSB, 360, 100, 100, 100);
+  maxLat = Math.max(...data.stations.map( o => o.lat));
+  minLat = Math.min(...data.stations.map( o => o.lat));
+  maxLng = Math.max(...data.stations.map( o => o.lng));
+  minLng = Math.min(...data.stations.map( o => o.lng));
+  
+  createCanvas(700, 700);
+  
+  data.stations.forEach(s => {
+    const station = new Station(s);
+    if (station.isValid) {
+      stations.push(station);
+    }
+  });
+
+  data.trips.forEach(t => {
+    const trip = new Trip(t);
+    if (trip.isValid) {
+      trips.push(trip);
+    }
+  });
+
+  currentTime = parseInt(trips[0].startedTime);
+  maxTime = parseInt(trips[trips.length-1].endedTime);
+  timeSlider = createSlider(currentTime, maxTime, currentTime);
+  timeSlider.size(width - 100);
+  createButton('Toggle Animation').mousePressed(() => {
+    isPlaying = !isPlaying;
+  });
+}
+
+function draw() {
+  if (isPlaying) {
+    updateTime();
+  } else {
+    currentTime = timeSlider.value();
+  }
+  update();
+}
+
+function updateTime() {
+  if (currentTime < maxTime) {
+    currentTime += 50000;
+  }
+}
+
+function update() {
+  background(0, 0, 95);
+  stations.forEach(s => {
+    s.display();
+  });
+
+  trips.filter( t => t.isPresent(currentTime) ).forEach( t => {
+    t.display(currentTime);
+  });
+
+  const time = new Date(currentTime);
+  const timeStr = formatAMPM(time);
+  fill(0);
+  textSize(20);
+  text (timeStr, 50, 50);
+  timeSlider.value(currentTime);
+}
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+
---
+++ examples/data-viz/01-citibike-steering/trip.js
@@ -0,0 +1,44 @@
+class Trip {
+  constructor(trip) {
+    this.isValid = true;
+    this.startStation = stations.find( s => 
+      s.id === trip.start_station_id 
+    );
+    this.endStation = stations.find( s => 
+      s.id === trip.end_station_id 
+    );
+    
+    if (!this.startStation || !this.endStation || this.startStation.id === this.endStation.id) {
+      this.isValid = false;
+      return;
+    }
+
+    //this.angle = this.startStation.getPos().angleBetween(this.endStation.getPos());
+    this.angle = atan2(this.endStation.getPos().y - this.startStation.getPos().y, this.endStation.getPos().x - this.startStation.getPos().x);
+    this.hue = map(this.angle, 0, PI, 0, 360);
+    this.startedTime = parseInt(trip.st);
+    this.endedTime = parseInt(trip.et);
+  }
+
+  isPresent(currentTime) {
+    return currentTime > this.startedTime && currentTime < this.endedTime;
+  }
+
+  display(currentTime) {
+    const tripProgress = map(currentTime, this.startedTime, this.endedTime, 0, 1);
+    const pos = this.startStation.getPos().lerp(this.endStation.getPos(), tripProgress);
+    const sineTripProgress = sqrt(sin(tripProgress * PI), 3);
+    const alpha = map(sineTripProgress, 0, 1, 0, 100);
+    noStroke();
+    
+    push();
+    fill(this.hue, 80, 70, alpha);
+    translate(pos.x, pos.y);
+    rotate(this.angle + HALF_PI);
+    const tSize = 3;
+    const tSharpness = 4;
+    triangle(0, 0, tSize, tSize * tSharpness, -tSize, tSize * tSharpness);
+    //rect (0, 0, 10, 20);
+    pop();
+  }
+}
