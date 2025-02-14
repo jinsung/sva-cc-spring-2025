f4ba7bb773054d474f844d1c22baba4ae7dc8705
Jin Sung Yoo <ujinsung@gmail.com>
2025-02-13 21:21:46 -0500

    week5 studio work

---
+++ examples/05/particle_system/particle-system.js
@@ -0,0 +1,28 @@
+class ParticleSystem {
+    constructor() {
+        this.particles = [];
+        this.gravity = createVector(0, 0.1);
+    }
+
+    addParticles(x, y, num) {
+        for (let i = 0; i < num; i++) {
+            const p = new Particle(x, y, random(5, 10));
+            const randomForce = p5.Vector.random2D();
+            randomForce.setMag(random(1, 5));
+            p.applyForce(randomForce);
+            this.particles.push(p);
+        }
+    }
+
+    loop() {
+        for (let i = this.particles.length-1; i >= 0; i--) {
+            const p = this.particles[i];
+            if (p.isDead()) {
+                this.particles.splice(i, 1);
+            }
+            p.applyForce(this.gravity);
+            p.update();
+            p.display();
+        }
+    }
+}
---
+++ examples/05/particle_system/particle.js
@@ -0,0 +1,55 @@
+class Particle {
+    constructor(x, y, r) {
+        this.pos = createVector(x, y);
+        this.vel = createVector();
+        this.acc = createVector();
+        this.r = r;
+        this.friction = 0.995;
+        this.maxAge = random(10, 100);
+        this.age = 0;
+    }
+
+    update() {
+        this.age++;
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+        this.vel.mult(this.friction);
+        //this.bounce();
+    }
+
+    display() {
+        noStroke();
+        const alpha = map(this.age, 0, this.maxAge, 255, 0);
+        fill(255, alpha);
+        circle(this.pos.x, this.pos.y, this.r * 2);
+    }
+
+    isDead() {
+        return this.age > this.maxAge;
+    }
+
+    applyForce(force) {
+        this.acc.add(force);
+    }
+
+    bounce() {
+        if(this.pos.y > height) {
+            this.pos.y = height;
+            this.vel.y *= -1;
+        } else if (this.pos.y < 0) {
+            this.pos.y = 0;
+            this.vel.y *= -1;
+        } 
+
+        if (this.pos.x > width) {
+            this.pos.x = width;
+            this.vel.x *= -1;
+        } else if (this.pos.x < 0) {
+            this.pos.x = 0;
+            this.vel.x *= -1;
+        }
+    }
+
+    
+}
