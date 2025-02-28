c7c3e92201f3037bb92aa33c004ef1c0e949e1db
Jin Sung Yoo <ujinsung@gmail.com>
2025-02-27 21:16:42 -0500

    week 7 studio

---
+++ examples/07/matterjs-basic/sketch.js
@@ -0,0 +1,37 @@
+let engine;
+let boxes = [];
+let boxSize = 30;
+
+function setup() {
+    createCanvas(600, 600);
+    rectMode(CENTER);
+    engine = Matter.Engine.create();
+    let ground = Matter.Bodies.rectangle(
+        width/2, height, width * 1.25, 25, {isStatic: true}
+    );
+    Matter.Composite.add(engine.world, ground);
+}
+
+function draw() {
+    background(190);
+    for(let i = 0; i < boxes.length; i++) {
+        const box = boxes[i];
+        push();
+        translate(box.position.x, box.position.y);
+        rotate(box.angle);
+        rect(0, 0, boxSize, boxSize);
+        pop();
+    }
+    Matter.Engine.update(engine, 1000/30);
+}
+
+function addBox(x, y) {
+    const box = Matter.Bodies.rectangle(
+        x, y, boxSize, boxSize);
+    Matter.Composite.add(engine.world, box);
+    boxes.push(box);
+}
+
+function mousePressed() {
+    addBox(mouseX, mouseY);
+}
